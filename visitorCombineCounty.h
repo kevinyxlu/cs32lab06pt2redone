2fb1cfc99f422ab71c6297644e31717c361fa555
Kevin Lu <klu@csilvm-20.cs.ucsb.edu>
2021-05-25 21:10:45 -0700

    d

--- visitorCombineCounty.h
+++ visitorCombineCounty.h
@@ -16,10 +16,23 @@ public:
     visitorCombineCounty() {
     }
 
+    string cleanCountyName(string countyName)
+    // if " County" is appended to a string, it removes " County"
+    {
+        string cleanName = countyName;
+        string toClean = " County";
+        size_t found = cleanName.find(toClean);
+        if(found != std::string::npos)
+        {
+            cleanName.erase(found, found+6);
+        }
+        return cleanName;
+    }
+
     //store demographic data by county name
 	void visit(shared_ptr<demogData> obj) {
         //fill in
-        string county = obj->getRegionName();
+        string county = cleanCountyName(obj->getRegionName()) + obj->getState();
         if(allComboDemogData.count(county)) // if the state exists in the map already, then aggregate
         {
             allComboDemogData[county]->addDemogData(obj);
@@ -29,7 +42,7 @@ public:
     
     //aggregate police shooting data by county
     void visit(shared_ptr<psData> obj) {
-        string county = obj->getRegionName();
+        string county = cleanCountyName(obj->getRegionName()) + obj->getState();
         if(allComboPoliceData.count(county)) // if the state exists in the map already, then aggregate
         {
             allComboPoliceData[county]->addPSData(obj);
